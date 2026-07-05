5247c7cd850824cca2edcf1c8413871a6019c143
hddq <hddqgit@gmail.com>
2026-07-06 00:49:43 +0200

    chore: setup custom domain CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: repoName ? `/${repoName}/` : '/',
 })
